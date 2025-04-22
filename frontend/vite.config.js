895084572b285e651d3d8491730a1da1de5ea90c
Guillermo Fontana <guilleefontana@gmail.com>
2025-04-22 17:16:11 -0300

    integración de taildwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite';
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   root: './',
   base: isProduction ? '/static/' : '/', // ← clave
   build: {
